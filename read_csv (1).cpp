c4577fdcd8914f29f6ec5caebcc7a653e35944b3
taynara <taynaracris11@hotmail.com>
2021-08-28 21:19:03 -0300

    erase e replace

--- read_csv (1).cpp
+++ read_csv (1).cpp
@@ -55,7 +55,7 @@ int main(){
     nota5.open("Nota5.txt");
 
     string line;
-    string texto, rate;
+    string texto = "", rate = "";
     string separator = ",   ,"; 
 
     int inicio_separator;
@@ -63,7 +63,10 @@ int main(){
     string avaliacao;
 
     while( file.good() ) {
-        // Pega uma line do arquivo
+        rate = "";
+	texto = "";
+
+	// Pega uma line do arquivo
         getline(file, avaliacao);
 
         // Percorre a line inteira e troca as aspas por espaço em branco
@@ -71,37 +74,35 @@ int main(){
 
 	inicio_separator = encontra(avaliacao, separator);
 
-        // Pegar uma substring de 0 até onde começa o separador
-        texto = avaliacao.substr(0, inicio_separator);
-        // Apaga de 0 até onde termina o separador
-        avaliacao.erase(0, avaliacao.find(separator) + separator.length());
-        // Pega o que sobrou que é a nota e guarda no rate
-        rate = avaliacao;
-
-        if(rate == "1") {
+	for(int i = 0; i < inicio_separator; i++){
+	    texto += avaliacao[i];
+	}
+	for(int i = inicio_separator + separator.length(); i <= avaliacao.length(); i++){
+	    rate += avaliacao[i];
+	}
+        if(rate[0] == '1') {
             // Escreve o textoo no arquivo
             nota1 << texto; 
             // Escrevo uma quebra de line no arquivo
             nota1 << endl;
         }
-	else if(rate == "2"){
+	else if(rate[0] == '2'){
 	    nota2 << texto;
 	    nota2 << endl;
 	}
-	else if(rate == "3"){
+	else if(rate[0] == '3'){
 	    nota3 << texto;
 	    nota3 << endl;
 	}
-	else if(rate == "4"){
+	else if(rate[0] == '4'){
 	    nota4 << texto;
 	    nota4 << endl;
 	}
-	else if(rate == "5"){
+	else if(rate[0] == '5'){
 	    nota5 << texto;
 	    nota5 << endl;
 	}
     }
-
     file.close();
 
     nota1.close();
