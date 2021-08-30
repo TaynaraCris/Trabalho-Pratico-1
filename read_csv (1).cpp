7ae1f9ac16734bb38bc6e40831b2c0f9fde1abcc
taynara <taynaracris11@hotmail.com>
2021-08-30 15:32:59 -0300

    segunda parte

--- read_csv (1).cpp
+++ read_csv (1).cpp
@@ -23,6 +23,30 @@ int encontra(string avaliacao, string separator){
     return -1;
 }
 
+
+string * get_tokens(string str){
+     static string tokens[2000];
+     string s_token;
+     int cont = 0;
+     const char* separator = " ";
+ 
+     char *token = strtok(const_cast<char*>(str.c_str()), separator);
+     while (token != nullptr)
+     {
+         s_token = string(token);
+         if (s_token.size() > 3){
+             tokens[cont] = s_token;
+             cont++;
+         }
+ 
+         token = strtok(nullptr, separator);
+     }
+ 
+     return tokens;
+ }
+
+
+
 string replace(string avaliacao){
 
     for(int i =  0; i < avaliacao.length(); i++){
@@ -43,7 +67,7 @@ int main(){
     nota1.open("Nota1.txt");
 
     ofstream nota2;
-    nota2.open("Nota2.txt");
+    nota2.open("Nota2.txt");	
 
     ofstream nota3;
     nota3.open("Nota3.txt");
@@ -80,6 +104,25 @@ int main(){
 	for(int i = inicio_separator + separator.length(); i <= avaliacao.length(); i++){
 	    rate += avaliacao[i];
 	}
+
+
+	for(int i = 0; i < texto.length(); i++){
+	    if(texto[i] == ',' || texto[i] == '.' || texto[i] == '-' || texto[i] == '_' || texto[i] == '!' || texto[i] == '?'){
+	       texto[i] = ' ';
+	    }
+	    else{
+	       texto[i] = texto[i];
+	    }
+	}
+	for(int i = 0; i < texto.length(); i++){
+	    if(texto[i] == ' ' && texto[i+1] == ' '){
+	       texto[i] = ' ';
+	    }
+	    else{
+	       texto[i] = texto[i];
+	    }
+	}
+
         if(rate[0] == '1') {
             // Escreve o textoo no arquivo
             nota1 << texto; 
@@ -111,43 +154,67 @@ int main(){
     nota4.close();
     nota5.close();
 
-    //INICO DO SEGUNDO TOPICO DO TRABALHO
-    FILE *Vocabulario;
-    Vocabulario = fopen("tripadvisor_hotel_reviews.csv","r");
+    //Reopen files
+     ifstream read_nota1("Nota1.txt");
+     ifstream read_nota2("Nota2.txt");
+     ifstream read_nota3("Nota3.txt");
+     ifstream read_nota4("Nota4.txt");
+     ifstream read_nota5("Nota5.txt");
 
-    struct p
-    {
-        char palavra[1000];
-    };
-    
-    
-    char string[1000];
-    int tmp;
-    p vetor[1000];
-    int j = 0;
+     string text_line;
+     string * tokens_nota_1;
+     string * tokens_nota_2;
+     string * tokens_nota_3;
+     string * tokens_nota_4;
+     string * tokens_nota_5;
 
-    while (!feof(Vocabulario) && !ferror(Vocabulario)) {
-        tmp = fgetc(Vocabulario);
-        int i = 0;
+     int cont;
+ 
+     while(getline(read_nota1, text_line)) {
+         tokens_nota_1 = get_tokens(text_line);
+     }
 
-        while ((tmp != ' ') && (tmp != ',') && (tmp != ';') && (tmp != '.') && (tmp != 49) && (tmp != 50) && (tmp != 51) && (tmp != 52) && (tmp != 53) && (tmp != 34))
-        {
-            string[i] = tmp;
-            tmp = fgetc(Vocabulario);
-            if(tmp == EOF){
-                break;
-            }
-            i++;
-        }
-        
-        string[i] = '\0'; 
-        strcpy(vetor[j].palavra,string);
-        printf("%s\n",vetor[j].palavra);
-        j++;
-    }
+     for ( int i = 0; i < 10; i++ ) {
+         cout << tokens_nota_1[i]<< endl;
+     }
 
+     while(getline(read_nota2, text_line)) {
+         tokens_nota_2 = get_tokens(text_line);
+     }
 
-    fclose(Vocabulario);
+     for ( int i = 0; i < 10; i++ ) {
+         cout << tokens_nota_2[i]<< endl;
+     }
+
+     while(getline(read_nota3, text_line)) {
+         tokens_nota_3 = get_tokens(text_line);
+     }
+
+     for ( int i = 0; i < 10; i++ ) {
+         cout << tokens_nota_3[i]<< endl;
+     }
+     
+     while(getline(read_nota4, text_line)) {
+         tokens_nota_4 = get_tokens(text_line);
+     }
+
+     for ( int i = 0; i < 10; i++ ) {
+         cout << tokens_nota_4[i]<< endl;
+     }
+     while(getline(read_nota5, text_line)) {
+         tokens_nota_5 = get_tokens(text_line);
+     }
+
+     for ( int i = 0; i < 10; i++ ) {
+         cout << tokens_nota_5[i]<< endl;
+     }
+ 
+ 
+     read_nota1.close();
+     read_nota2.close();
+     read_nota3.close();
+     read_nota4.close();
+     read_nota5.close();
 
     return 0;
 }
